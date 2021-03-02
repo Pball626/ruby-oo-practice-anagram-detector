9e679f96d6c06e14bc8665eed1e6d98723e4419a
joey <pball626@gmail.com>
2021-03-01 23:20:49 -0600

    commit tree 6c23a6a75e2daa2f800ce85160166094afddd4c1

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+require "pry" 
+class Anagram
+    attr_accessor :listen, :diaper, :ba, :allergy
+    
+    def initialize
+        @listen = listen
+        @diaper = diaper
+        @ba = ba
+        @allergy = allergy
+    end
+
+    def detector
+        
+    end
+
+
+
+    
+    # binding.pry
+end
